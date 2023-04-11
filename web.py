4a6685cab38489391388b6b97da644797e627568
Admin <zhonglinokami@gmail.com>
2023-04-11 22:14:36 +0700

    push

---
+++ web.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+import openai
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('html.html')
+@app.route('/translate', methods=['POST'])
+def translate():
+    source_text = request.form['source_text']
+    source_lang = request.form['source_lang']
+    target_lang = request.form['target_lang']
+
+    openai.api_key = "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"
+    model_engine = "text-davinci-002"
+
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt="translate " + source_text + " from " + source_lang + " to " + target_lang,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    target_text = response['choices'][0]['text'].strip()
+
+    return render_template('html.html', source_text=source_text, target_text=target_text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
